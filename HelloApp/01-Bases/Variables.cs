ffe7a05bb530bb3ebea1ae54e804934ab785f011
Hector <hmreluz@gmail.com>
2025-08-22 22:03:42 -0500

    01-Basic-Syntax: Hello project: declare and initialize variables

---
+++ HelloApp/01-Bases/Variables.cs
@@ -0,0 +1,11 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int age = 30;
+        string name = "Bruce";
+        bool isStudent = false;
+        
+        Console.WriteLine($"Name: {name}, Age: {age},  Student: {isStudent}");
+    }
+}
