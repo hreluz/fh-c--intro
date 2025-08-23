2a06d4b2c9f127e5d5957b3957e2ff1ea6945dbd
Hector <hmreluz@gmail.com>
2025-08-23 09:30:19 -0500

    01-Basic-Syntax: Hello project: type by value and reference

---
+++ HelloApp/01-Bases/TypeDIfference.cs
@@ -0,0 +1,22 @@
+partial class Program
+{
+    static void TypeDifference()
+    {
+        int x = 5;
+        int y = x;
+
+        y = 15;
+        Console.WriteLine($"x:{x}, y:{y}");
+
+        Person person1 = new Person { Name = "Bruce" };
+        Person person2 = person1;
+
+        person2.Name = "Tim";
+        Console.WriteLine($"person1.Name: {person1.Name}");
+    }
+}
+
+class Person
+{
+    public string? Name { get; set; }
+}
