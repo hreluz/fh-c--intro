a9f4fd1cfecf07c9ad4ccca278b7c9e258992e3b
Hector <hmreluz@gmail.com>
2025-08-23 09:24:00 -0500

    01-Basic-Syntax: Hello project: days to next birthday

---
+++ HelloApp/01-Bases/HomeWork2Days.cs
@@ -0,0 +1,30 @@
+using System.Globalization;
+
+partial class Program
+{
+    static void LivedDays()
+    {
+        DateTime birthDate = new DateTime(1963, 08, 10);
+        TimeSpan difference = DateTime.Now - birthDate;
+
+        Console.WriteLine($"You have lived {difference.Days} days");
+    }
+
+    static void DaysUntilNextBirthday()
+    {
+        Console.WriteLine("Enter your birthdate (dd/mm/aaaa): ");
+        string? birthDateString = Console.ReadLine()!;
+        DateTime birthDate = DateTime.ParseExact(birthDateString, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+        DateTime currentDate = DateTime.Now.Date;
+        DateTime nextBirthday = new DateTime(currentDate.Year, birthDate.Month, birthDate.Day);
+
+        if (nextBirthday < currentDate)
+        {
+            nextBirthday = nextBirthday.AddYears(1);
+        }
+
+        int daysRemaining = (nextBirthday - currentDate).Days;
+        Console.WriteLine($"You will need to wait {daysRemaining} for your next birthdate");
+    }
+}
