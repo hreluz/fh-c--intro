99640885194462054c07b71feb99661310397344
Hector <hmreluz@gmail.com>
2025-08-23 09:49:00 -0500

    01-Basic-Syntax: Hello project: operators

---
+++ HelloApp/01-Bases/Operators.cs
@@ -0,0 +1,29 @@
+partial class Program
+{
+    static void Operators()
+    {
+        int number = 15;
+
+        bool isEven = number % 2 == 0;
+        bool isGreaterThanTen = number > 10;
+
+
+        if (isEven && isGreaterThanTen)
+        {
+            Console.WriteLine($"The number {number} is even and greater than 10");
+        }
+        else if (!isEven && isGreaterThanTen)
+        {
+            Console.WriteLine($"The number {number} is odd and greater than 10");
+
+        }
+        else
+        {
+            Console.WriteLine($"The number does not meet the conditions");
+        }
+
+        int age = 21;
+        string category = age > 18 ? "Adult" : "Young";
+        Console.WriteLine(category);
+    }
+}
