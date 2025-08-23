450aa25fac187f8afac3def1b42b34ba088db847
Hector <hmreluz@gmail.com>
2025-08-23 13:20:49 -0500

    01-Basic-Syntax: Hello project: class, structs and records

---
+++ HelloApp/01-Bases/DataStructures.cs
@@ -0,0 +1,33 @@
+partial class Program
+{
+    static void DataStructures()
+    {
+        User batman = new User { Name = "Bruce", Age = 30 };
+        batman.Greet();
+
+        Point point = new Point { X = 301, Y = 200 };
+        Console.WriteLine($"Point ({point.X}, {point.Y})");
+
+        CellPhone cellPhone = new CellPhone("Sony Ericson x8", 2008);
+        Console.WriteLine(cellPhone);
+    }
+}
+
+class User
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, I am the user {Name}, and I am {Age} years old");
+    }    
+}
+
+struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+record CellPhone(string Model, int Year);
