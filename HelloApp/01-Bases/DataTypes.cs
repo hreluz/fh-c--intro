4da9c44dd6f33a2a832727ff6799fe820b8ecaf8
Hector <hmreluz@gmail.com>
2025-08-22 23:25:04 -0500

    01-Basic-Syntax: Hello project: data types

---
+++ HelloApp/01-Bases/DataTypes.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+    static void DataTypes()
+    {
+        int integer = 42;
+        double decimalNumber = 3.1416;
+        bool isTrue = true;
+        char character = 'C';
+        string text = "Hola C#";
+        Console.WriteLine($"Integer{integer}, Decimal: {decimalNumber}, Boolean: {isTrue}, Character: {character}, Text: {text}");
+    }
+}
