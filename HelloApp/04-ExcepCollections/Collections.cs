d2c27588c83380b20ca54a46278d733be9617568
Hector <hmreluz@gmail.com>
2025-08-28 20:39:25 -0500

    03-Exceptions-n-LINQ: Hello project: collections dictionary and hashset

--- HelloApp/04-ExcepCollections/Collections.cs
+++ HelloApp/04-ExcepCollections/Collections.cs
@@ -8,6 +8,38 @@ partial class Program
         ShowNames(names);
         names.Remove("Bruce");
         ShowNames(names);
+
+        Dictionary<int, string> students = new()
+        {
+            {1, "Bruce"},
+            {2, "Clark"},
+            {3, "Lex"},
+        };
+
+        students.Add(4, "Scarecrow");
+
+        ShowStudents(students);
+
+        students.Remove(1);
+
+        ShowStudents(students);
+
+        HashSet<string> users = ["Bruce", "Clark", "Diana"];
+        users.Add("Lex");
+        users.Add("Bruce");
+
+        foreach (var user in users)
+        {
+            WriteLine(user);
+        }
+    }
+
+    private static void ShowStudents(Dictionary<int, string> students)
+    {
+        foreach (var student in students)
+        {
+            WriteLine($"Key: {student.Key}, Value: {student.Value}");
+        }
     }
 
     private static void ShowNames(List<string> names)
