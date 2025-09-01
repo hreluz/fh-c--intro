d2e1e557ab1bff3a6d04206c1ba4063e989a4761
Hector <hmreluz@gmail.com>
2025-08-31 20:05:12 -0500

    07-TaskMasterAPI-with.NET: create task

--- TaskMasterAPI/Controllers/TaskController.cs
+++ TaskMasterAPI/Controllers/TaskController.cs
@@ -28,4 +28,20 @@ public class TaskController : ControllerBase
         return Ok(task);
     }
 
+    [HttpPost]
+    public ActionResult<Models.Task> CreateTask(Models.TaskInsert taskInsert)
+    {
+        var newTask = new Models.Task
+        {
+            Id = TaskDataStore.Current.Tasks.Max(t => t.Id) + 1,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            IsCompleted = false,
+            Title = taskInsert.Title,
+            Description = taskInsert.Description,
+        };
+
+        TaskDataStore.Current.Tasks.Add(newTask);
+        return Ok(newTask);
+    }
 }
